e3d9c645b07fed2664618419fe30a0f71467f83f
ivansteb <steblerivan@gmail.com>
2024-05-25 19:13:43 -0300

    useContext

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,13 @@ import { useState } from 'react';
 import { Products } from './components/Products';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { useFilters } from './hooks/useFilters';
 
-function useFilters () {
-
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0
-  });
-
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      return ( 
-        product.price >= filters.minPrice &&
-        (
-          filters.category == 'all' ||
-          product.category == filters.category
-        )
-      )
-    })
-  };
-
-  return { filters, filterProducts, setFilters };
-}
 
 function App () {
   const [products] = useState(initialProducts)
   const { filters, filterProducts, setFilters } = useFilters()
+
   const filteredProducts = filterProducts(products)
 
   return (
