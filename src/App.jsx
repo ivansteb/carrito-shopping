376ca60928584ff4be5443dae7456009503d0015
ivansteb <steblerivan@gmail.com>
2024-05-25 17:11:14 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { products as initialProducts } from './mocks/products';
+import { useState } from 'react';
+import { Products } from './components/Products';
+import { Header } from './components/Header';
+
+function App() {
+  const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return ( 
+        product.price >= filters.minPrice &&
+        (
+          filters.category == 'all' ||
+          product.category == filters.category
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
+
+  return (
+    <>
+      <Header />
+      <Products products={filteredProducts}/>
+    </>   
+  )
+}
+
+export default App;
---
+++ src/components/Filters.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import './Filters.css';
+
+export function Filters ({ onChange }) {
+    const [minPrice, setMinPrice] = useState(0);
+
+    const handleChangeMinPrice = (event) => {
+        setMinPrice(event.target.value)
+        onChange(prevState => ({
+            ...prevState,
+            minPrice: event.target.value
+        }))
+    };
+
+    const handleChangeCategory = (event) => {
+        onChange(prevState => ({
+            ...prevState,
+            category: event.target.value
+        }))
+    };
+
+    return (
+        <section className="filters">
+            <div>
+                <label htmlFor='price'>Precio a partir de:</label>
+                <input
+                    type='range'
+                    id='price'
+                    min='0'
+                    max='1000'
+                    onChange={handleChangeMinPrice}
+                />
+                <span>${minPrice}</span>
+            </div>
+            
+            <div>
+                <label htmlFor='category'>Categoría</label>
+                <select id='category'>
+                    <option value='all'>Todas</option>
+                    <option value='kitchen'>Cocina</option>
+                    <option value='electronic'>Electrónico</option>
+                </select>
+            </div>
+        </section>
+    )
+}
