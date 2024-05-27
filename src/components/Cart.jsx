77d7d37c843a1735be07fac27e0ea8cdf66df937
ivansteb <steblerivan@gmail.com>
2024-05-27 11:46:57 -0300

    Mostrando la info del carrito

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,32 @@
 import './Cart.css';
-
-import { CartIcon, ClearCartIcon } from "./Icons";
+import { useCart } from '../hooks/useCart';
 import { useId } from "react";
+import { CartIcon, ClearCartIcon } from "./Icons";
+
+function CartItem ({ thumbnail, price, title, quantity, addToCart }) {
+    return (
+        <li>
+            <img 
+                src={thumbnail}
+                alt={title}
+            />
+            <div>
+                <strong>{title}</strong> - ${price}
+            </div>
+
+            <footer>
+                <small>
+                    Cant.: {quantity}
+                </small>
+                <button onClick={addToCart}>+</button>
+            </footer>
+        </li>
+    )
+}
 
 export function Cart () {
     const cartCheckboxId = useId();
+    const { cart, clearCart, addToCart } = useCart();
 
     return (
         <>
@@ -15,25 +37,16 @@ export function Cart () {
 
             <aside className="cart">
                 <ul>
-                    <li>
-                        <img 
-                            src='https://http2.mlstatic.com/D_NQ_NP_932343-MLA48829573698_012022-O.webp'
-                            alt='carrito'
+                    {cart.map(product => (
+                        <CartItem 
+                            key={product.id} 
+                            addToCart={() => addToCart(product)}
+                            {...product}
                         />
-                        <div>
-                            <strong>Carrito</strong> - $54
-                        </div>
-
-                        <footer>
-                            <small>
-                                Qty: 1
-                            </small>
-                            <button>+</button>
-                        </footer>
-                    </li>
+                    ))}
                 </ul>
 
-                <button>
+                <button onClick={clearCart}>
                     <ClearCartIcon />
                 </button>
             </aside>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,22 +3,13 @@ import { useFilters } from '../hooks/useFilters'
 import './Footer.css'
 
 export function Footer () {
-    const { filters } = useFilters();
-    const { cart } = useCart();
 
     return (
         <footer className='footer'>
-            {
-                JSON.stringify(cart, null, 2)
-            }
-            {
-            /*
             <h4>
                 Práctica en React － <span>@ivansteb</span>
             </h4>
             <h5>Carrito de compras con useContext & useReducer</h5>
-            */
-            }
         </footer>
     )
 }
